1d999faeb878d7133b0c234ea24fc4f76447fa4f
ramathmika <ramathmikavs1999@gmail.com>
2020-09-02 00:02:35 +0530

    Lab 2

---
+++ Lab 2/q1/q1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Enter two numbers: ");
+            int.TryParse(Console.ReadLine(), out num1);
+            int.TryParse(Console.ReadLine(), out num2);
+            Console.WriteLine("The results of arithmetic operations on the two numbers are: \n{0} + {1} = {2}\n{0} - {1} = {3}\n{0} * {1} = {4}\n{0} / {1} = {5}", num1, num2, num1+num2, num1-num2, num1*num2, num1/num2);
+            Console.Read();
+        }
+    }
+}
