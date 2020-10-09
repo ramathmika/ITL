3a2573789e97c315b93b52062916f48ffd4c799b
ramathmika <ramathmikavs1999@gmail.com>
2020-10-10 01:36:05 +0530

    Lab 4

---
+++ Lab 4/q2/q2/Employee.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace q2
+{
+    public partial class Employee : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.IsPostBack == false)
+            {
+                //Add Employee IDs to the dropdown list
+                EmployeeIDList.Items.Add("1001");
+                EmployeeIDList.Items.Add("1002");
+                EmployeeIDList.Items.Add("1003");
+                EmployeeIDList.Items.Add("1004");
+                EmployeeIDList.Items.Add("1005");
+
+                //EmployeeIDList.Items.Insert(0, new ListItem(string.Empty, string.Empty));
+                EmployeeIDList.SelectedIndex = -1;
+            }
+        }
+        protected void UpdateInfo(Object sender, EventArgs e)
+        {
+            string imageurl = "~/Images/" + EmployeeIDList.SelectedItem.Text + ".jpg";
+            EmployeeImage.ImageUrl = imageurl;
+        }
+
+        protected void PrintResult(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Today;
+            int year = date.Year;
+            string[] data = DOJTextBox.Text.Split('-');
+            var JoinedYear = data[0];
+            EmployeeTextBox.Text = JoinedYear;
+            if (year - Int32.Parse(JoinedYear) > 5)
+                ResultLabel.Text = "YES";
+            else
+                ResultLabel.Text = "NO";
+
+        }
+    }
+}
---
+++ Lab 4/q4/q4/captcha.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace q4
+{
+    public partial class captcha : System.Web.UI.Page
+    {
+        static int count;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(this.IsPostBack == false)
+            {
+                count = 0;
+            }
+        }
+
+        protected void AttemptMade(object sender, EventArgs e)
+        {
+            count++;
+            if(count < 3)
+            {
+                string attempt = CaptchaTextBox.Text;
+                if(attempt == "T4Y9")
+                {
+                    ResultLabel.Text = "Correct!";
+                }
+                else
+                {
+                    string res = "Try again. " + (3 - count).ToString() + " attempts left.";
+                    ResultLabel.Text = res;
+                    CaptchaTextBox.Text = "";
+                }
+            }
+            else
+            {
+                CaptchaTextBox.Visible = false;
+                ResultLabel.Text = "Too many wrong attempts.";
+            }
+        }
+    }
+}
