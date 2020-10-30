d6cc8d02deb14c2ffc7f4971dc9ebee41ea13db4
ramathmika <ramathmikavs1999@gmail.com>
2020-10-30 18:04:49 +0530

    Lab 6

---
+++ Lab 6/q1/q1/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace q1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DropDownList1.SelectedIndex = -1;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                Label1.Text = "Successfully Submitted";
+            }
+        }
+
+        protected void val_Phone(object source, ServerValidateEventArgs e)
+        {
+            string num = TextBox3.Text;
+            double t;
+            if (num.Length == 10 && double.TryParse(num, out t))
+            {
+                e.IsValid = true;
+            }
+            else
+                e.IsValid = false;
+        }
+    }
+}
