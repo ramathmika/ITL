965d05574dba33ac922cace26c8dc30be64b9c99
ramathmika <ramathmikavs1999@gmail.com>
2020-12-10 13:08:41 +0530

    10/12/2020

---
+++ Lab 10/q3/q3/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.IO;
+
+namespace q3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string file = Path.Combine(Request.PhysicalApplicationPath, @"App_Data\Cars.xml");
+
+            XDocument doc = new XDocument(
+                new XElement("Cars", 
+                    new XElement("Detail", 
+                        new XElement("Name", "I20"), 
+                        new XElement("Make", "Hyundai"), 
+                        new XElement("Price", "700000")
+                    ),
+                    new XElement("Detail",
+                        new XElement("Name", "I20"),
+                        new XElement("Make", "Hyundai"),
+                        new XElement("Price", "700000")
+                    )
+                )
+            );
+
+            doc.Save(file);
+        }
+    }
+}
---
+++ Lab 10/q4/q4/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="q4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Choose a drive: 
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server"></asp:ListBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Delete" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
